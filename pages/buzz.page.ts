6c1205bb35dba516f539d7eb5c8cc33afb93931e
Felipe Vilar <felipe.bvilar@gmail.com>
2026-08-09 20:14:14 -0300

    cenario de anexar photo

--- pages/buzz.page.ts
+++ pages/buzz.page.ts
@@ -5,36 +5,68 @@ export class BuzzPage {
     private readonly pageSubtitleBuzzNews: Locator;
 
     private readonly txtSuccessMessagePosted: Locator;
+    private readonly txtModalSharePhotos: Locator;
 
     private readonly buttonMenuBuzz: Locator;
     private readonly buttonNewPost: Locator;
+    private readonly buttonSharePhotos: Locator;
+    private readonly buttonAddPhotos: Locator;
+    private readonly buttonConfirmShare: Locator;
 
     private readonly postInput:Locator;
+    private readonly photoPostInput:Locator;
+
+    private readonly photoInput: Locator;
 
     constructor(private readonly page: Page){
         this.pageTitleBuzz = page.getByRole('heading', { name: 'Buzz' });
         this.pageSubtitleBuzzNews = page.getByText('Buzz Newsfeed', { exact: true });
 
         this.txtSuccessMessagePosted = page.getByText('Successfully Saved');
+        this.txtModalSharePhotos = page.getByRole('dialog').getByText('Share Photos', { exact: true });
 
         this.buttonMenuBuzz = page.getByRole('link', { name: 'Buzz' });
         this.buttonNewPost = page.getByRole('button', { name: 'Post', exact: true });
+        this.buttonSharePhotos = page.getByRole('button', { name: 'Share Photos', exact: true});
+        this.buttonAddPhotos = page.getByText('Add Photos', { exact: true, });
+        this.buttonConfirmShare = page.getByRole('dialog').getByRole('button', { name: 'Share', exact: true});
 
         this.postInput = page.getByPlaceholder("What's on your mind?");
+        this.photoPostInput = page.getByRole('dialog').getByPlaceholder("What's on your mind?");
+
+        this.photoInput = page.locator('.orangehrm-photo-input').locator('input[type="file"]');
     }
 
-    async createNewPost(message: string): Promise<void> {
+    async writeNewPost(message: string): Promise<void> {
         await this.postInput.fill(message);
+    }
+    async clickPost(){
         await this.buttonNewPost.click();
     }
 
-    async validateMessageSucessPost() {
+    async openModalSharePhoto(){
+        await this.buttonSharePhotos.click()
+        await expect(this.txtModalSharePhotos).toBeVisible({
+            timeout: 10000,
+    });
+       await expect(this.buttonConfirmShare).toBeDisabled();
+    }
+
+    async insertFileInPost(filePath: string, message: string): Promise<void>{
+        await this.photoPostInput.fill(message);
+        await this.photoInput.setInputFiles(filePath);
+        await expect(this.buttonConfirmShare).toBeEnabled();
+        await this.buttonConfirmShare.click();
+    }
+
+    async validateMessageSuccessPost() {
         await expect(this.txtSuccessMessagePosted).toBeVisible();
     }
 
     async validatePostCreated(message: string): Promise<void> {
-        const newPost = this.page.locator('p.orangehrm-buzz-post-body-text')
-        .filter({ hasText: message });
+        const newPost = this.page.getByText(message, {
+            exact: true,
+        });
         await expect(newPost).toBeVisible({
             timeout: 10000,
         });
--- tests/e2e/buzz.spec.ts
+++ tests/e2e/buzz.spec.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { test } from '../../fixtures/app.fixture';
 
 test.describe('Buzz Screen', () => {
@@ -19,18 +20,17 @@ test.describe('Buzz Screen', () => {
         });
     });
 
-
-
-    test('should create a new post sucessfully', async ({
+    test('CT01 - create new post text-only sucessfully', async ({
         buzzPage,
         logger,
     }) => {
-        const message = `Automated post - ${Date.now()}`;
+        const message = `Automated post only-text - ${Date.now()}`;
 
         await test.step('Create a new post', async () => {
             logger.info(`Creating new post: ${message}`);
-            await buzzPage.createNewPost(message);
-            await buzzPage.validateMessageSucessPost();
+            await buzzPage.writeNewPost(message);
+            await buzzPage.clickPost();
+            await buzzPage.validateMessageSuccessPost();
             logger.success(`Post created with success`)
         });
 
@@ -40,4 +40,48 @@ test.describe('Buzz Screen', () => {
             logger.success(`Message posted with success`);
         });
     });
+
+    test('CT02 - Create new post with an image', async ({
+        buzzPage,
+        logger,
+    }) => {
+        const message = `Automated post with image- ${Date.now()}`;
+        const imagePath = path.resolve(
+            'test-data',
+            'files',
+            'test-valid.jpeg'
+        )
+
+        await test.step('Write a new post', async () => {
+            logger.info(`Writing new post: ${message} and attaching file`);
+            await buzzPage.openModalSharePhoto();
+            await buzzPage.insertFileInPost(imagePath, message);
+            // await buzzPage.validateMessageSuccessPost();
+            logger.success(`Post created and photo attach with success`)
+        });
+
+        await test.step('Validate success post created', async () => {
+            logger.info(`Validating success post created`);
+            await buzzPage.validatePostCreated(message);
+            logger.success(`Message posted with success`);
+        });
+    });
+
+    // test('CT03 - Create new post with invalid file', async ({
+    //     buzzPage,
+    //     logger,
+    // }) => {
+    //     await test.step('Create a new post', async () => {
+    //         logger.info(`Creating new post with file invalid`);
+    //         await buzzPage.createNewPost(message);
+    //         await buzzPage.validateMessageSuccessPost();
+    //         logger.success(`Post created with success`)
+    //     });
+
+    //     await test.step('Validate success post created', async () => {
+    //         logger.info(`Validating success post created`);
+    //         await buzzPage.validatePostCreated(message);
+    //         logger.success(`Message posted with success`);
+    //     });
+    // });
 });
